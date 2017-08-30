2285fbace718c5dc9452917261686e0838a1ad34
Ifiok Jr <ifiokotung@gmail.com>
2017-08-30 23:03:53 +0100

    docs: publish docs to gitbook
    Enable publishing of docs via gulp command.

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,6 +1,20 @@
 import { series, task } from 'gulp';
+import { deployGHPages } from './tasks/ghPages';
+import { gitbookBuild, gitbookInstall } from './tasks/gitbook';
 import { lintMarkdown } from './tasks/markdown';
 
 task('lint:markdown', series(lintMarkdown));
 const lintMarkdownTask = task('lint:markdown');
 lintMarkdownTask.description = 'Lint markdown files throughout the project';
+
+task('gitbook', series(gitbookInstall, gitbookBuild));
+const gitbookTask = task('gitbook');
+gitbookTask.description = 'Build and install gitbook';
+
+task('ghpages', series(deployGHPages));
+const ghPagesTask = task('ghpages');
+ghPagesTask.description = 'Deploy GitHub Pages';
+
+task('deploy:docs', series(gitbookTask, ghPagesTask));
+const deployDocsTask = task('deploy:docs');
+deployDocsTask.description = 'Deploy kj documentation';
